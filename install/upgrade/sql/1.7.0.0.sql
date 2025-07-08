f387d0b5546bea0a86f1a13cb76e117e6844e636
vishalsingh.qlo325 <vishalsingh.qlo325@webkul.in>
2025-07-08 14:53:08 +0530

    added 1.7.0 upgrade files

---
+++ install/upgrade/sql/1.7.0.0.sql
@@ -0,0 +1,100 @@
+SET NAMES 'utf8';
+
+INSERT INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_upd`) VALUES
+	('PS_ALLOW_ADD_ALL_SERVICES_IN_BOOKING', '1', NOW(), NOW()),
+	('PS_ALLOW_CREATE_CUSTOM_SERVICES_IN_BOOKING', '1', NOW(), NOW()),
+    ('PS_MAIL_SUBJECT_PREFIX', '1', NOW(), NOW()),
+    ('PS_CUSTOMER_SERVICE_REQUIRED_NAME', '1', NOW(), NOW()),
+    ('PS_CUSTOMER_SERVICE_DISPLAY_NAME', '1', NOW(), NOW()),
+    ('PS_CUSTOMER_SERVICE_REQUIRED_PHONE', '1', NOW(), NOW()),
+    ('PS_CUSTOMER_SERVICE_DISPLAY_PHONE', '1', NOW(), NOW()),
+    ('PS_CUSTOMER_SERVICE_DISPLAY_CONTACT', '1', NOW(), NOW()),
+    ('PS_STANDARD_PRODUCT_ORDER_ADDRESS_PREFRENCE', '1', NOW(), NOW()),
+    ('PS_CUSTOMER_GUEST_MAX_LIMIT', '1', NOW(), NOW()),
+    ('QLO_VERSION_DB', '1.7.0.0', NOW(), NOW()),
+    ('QLO_INSTALL_VERSION', '1.7.0.0', NOW(), NOW());
+
+ALTER TABLE `PREFIX_customer`
+    ADD `phone` VARCHAR(32) NULL AFTER `ape`;
+
+UPDATE `PREFIX_customer` c
+    JOIN `PREFIX_customer_guest_detail` cgd ON c.`email` = cgd.`email`
+    SET c.`phone` = cgd.`phone` WHERE cgd.`id_cart` = 0;
+
+DELETE FROM `PREFIX_customer_guest_detail` WHERE `id_cart` = 0;
+
+CREATE TABLE `PREFIX_cart_customer_guest` (
+    `id_customer_guest_detail` int(10) unsigned NOT NULL,
+    `id_cart` int(10) unsigned NOT NULL,
+    PRIMARY KEY (`id_customer_guest_detail`, `id_cart`)
+) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8 COLLATION;
+
+INSERT INTO `PREFIX_cart_customer_guest` (`id_customer_guest_detail`, `id_cart`)
+    SELECT `id_customer_guest_detail`, `id_cart` FROM `PREFIX_customer_guest_detail`;
+
+ALTER TABLE `PREFIX_customer_guest_detail`
+    ADD `id_customer` INT(10) unsigned NOT NULL AFTER `id_customer_guest_detail`;
+
+UPDATE `PREFIX_customer_guest_detail` cgd
+    LEFT JOIN `PREFIX_cart` c ON c.`id_cart` = cgd.`id_cart`
+    SET cgd.`id_customer` = c.`id_customer`;
+
+ALTER TABLE `PREFIX_customer_guest_detail`
+    DROP COLUMN `id_cart`;
+
+ALTER TABLE `PREFIX_customer_message`
+    ADD `id_product` INT(10) UNSIGNED NULL AFTER `message`;
+
+ALTER TABLE `PREFIX_customer_thread`
+    DROP INDEX `id_product`,
+    DROP COLUMN `id_product`,
+    ADD COLUMN `id_employee` INT(10) UNSIGNED DEFAULT NULL AFTER `id_customer`,
+    ADD COLUMN `user_name` VARCHAR(128) DEFAULT NULL AFTER `id_order`,
+    ADD COLUMN `phone` VARCHAR(32) DEFAULT NULL AFTER `user_name`,
+    ADD COLUMN `subject` TEXT DEFAULT NULL AFTER `phone`;
+
+ALTER TABLE `PREFIX_customer_thread`
+    ADD COLUMN `status_int` INT(10) NOT NULL DEFAULT 1;
+    UPDATE `PREFIX_customer_thread`
+
+SET `status_int` =
+    CASE `status`
+        WHEN 'open' THEN 1
+        WHEN 'closed' THEN 2
+        WHEN 'pending1' THEN 3
+        WHEN 'pending2' THEN 4
+        ELSE 1
+    END;
+
+ALTER TABLE `PREFIX_customer_thread`
+    DROP COLUMN `status`,
+    CHANGE COLUMN `status_int` `status` INT(10) NOT NULL DEFAULT 1;
+
+ALTER TABLE `PREFIX_order_detail`
+    CHANGE COLUMN `product_service_type` `selling_preference_type` TINYINT(1) NOT NULL DEFAULT '1';
+
+ALTER TABLE `PREFIX_order_return_detail`
+    ADD COLUMN `id_service_product_order_detail` INT(10) NOT NULL AFTER `id_htl_booking`
+    KEY `id_service_product_order_detail` (`id_service_product_order_detail`);
+
+ALTER TABLE `PREFIX_order_slip_detail`
+    DROP PRIMARY KEY,
+    ADD COLUMN `id_service_product_order_detail` INT(10) UNSIGNED NOT NULL AFTER `id_htl_booking`,
+    ADD PRIMARY KEY (`id_order_slip`, `id_htl_booking`, `id_service_product_order_detail`);
+
+ALTER TABLE `PREFIX_product`
+    CHANGE COLUMN `service_product_type` `selling_preference_type` TINYINT(1) NOT NULL DEFAULT '1';
+
+ALTER TABLE `PREFIX_specific_price`
+    DROP INDEX `id_product_2`,
+    ADD COLUMN `id_htl_cart_booking` INT UNSIGNED NOT NULL,
+    ADD UNIQUE KEY `id_product_2` (`id_product`,`id_product_attribute`,`id_customer`,`id_cart`,`from`,`to`,`id_shop`,`id_shop_group`,`id_currency`,`id_country`,`id_group`,`from_quantity`,`id_specific_price_rule`, `id_htl_cart_booking`);
+
+ALTER TABLE `PREFIX_shop_url`
+    MODIFY COLUMN `physical_uri` VARCHAR(256) NOT NULL,
+    MODIFY COLUMN `virtual_uri` VARCHAR(256) NOT NULL;
+
+
+/* PHP:add_our_properties_meta_page_170(); */;
+/* PHP:clean_hotel_description_170(); */;
+/* PHP:update_tabs_170(); */;
---
+++ modules/hotelreservationsystem/upgrade/upgrade-1.7.0.php
@@ -0,0 +1,326 @@
+<?php
+/**
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Open Software License version 3.0
+* that is bundled with this package in the file LICENSE.md
+* It is also available through the world-wide-web at this URL:
+* https://opensource.org/license/osl-3-0-php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to support@qloapps.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade this module to a newer
+* versions in the future. If you wish to customize this module for your needs
+* please refer to https://store.webkul.com/customisation-guidelines for more information.
+*
+* @author Webkul IN
+* @copyright Since 2010 Webkul
+* @license https://opensource.org/license/osl-3-0-php Open Software License version 3.0
+*/
+
+if (!defined('_PS_VERSION_'))
+	exit;
+
+function upgrade_module_1_7_0($objModule)
+{
+	$objUpgrade = new UpgradeHotelReservationSystem170($objModule);
+
+	return $objUpgrade->initUpgrade();
+}
+
+class UpgradeHotelReservationSystem170
+{
+	private $module;
+
+    public function __construct($module)
+    {
+        $this->module = $module;
+    }
+
+    public function initUpgrade()
+    {
+        return $this->callInstallTab()
+            && $this->updateDefaultConfiguration()
+            && $this->createHotelDefaultBedTypes()
+            && $this->updateTables();
+    }
+
+    public function callInstallTab()
+    {
+        return $this->installTab('AdminHotelBedTypes', 'Bed Types', 'AdminCatalog');
+    }
+
+    public function updateDefaultConfiguration()
+    {
+        Configuration::updateValue('WK_DISPLAY_CONTACT_PAGE_HOTEL_LIST', 0);
+        Configuration::updateValue('PS_MIN_BOOKING_OFFSET', Configuration::get('GLOBAL_PREPARATION_TIME'));
+
+        $dateToday = date('Y-m-d');
+        $maxOrderDate = Configuration::get('MAX_GLOBAL_BOOKING_DATE');
+        $checkoutOffset = $this->getNumberOfDays($dateToday, $maxOrderDate);
+
+        Configuration::updateValue('PS_MAX_CHECKOUT_OFFSET', $checkoutOffset);
+
+        $defaultDimensionUnitLang = array(
+            'en' => 'ft',
+            'nl' => 'ft',
+            'fr' => 'pi',
+            'de' => 'ft',
+            'ru' => 'фут',
+            'es' => 'ft',
+        );
+        $defaultDimensionUnit = array();
+        $languages = Language::getLanguages(false);
+        foreach ($languages as $lang) {
+            if (isset($htlTagLineLang[$lang['iso_code']])) {
+                $defaultDimensionUnit[$lang['id_lang']] = $defaultDimensionUnitLang[$lang['iso_code']];
+            } else {
+                $defaultDimensionUnit[$lang['id_lang']] = $defaultDimensionUnitLang['en'];
+            }
+        }
+
+        Configuration::updateValue('WK_DIMENSION_UNIT', $defaultDimensionUnit);
+
+
+        $keysToRemove = array(
+            'WK_HOTEL_GLOBAL_ADDRESS',
+            'WK_HOTEL_GLOBAL_CONTACT_EMAIL',
+            'WK_HOTEL_GLOBAL_CONTACT_NUMBER',
+            'MAX_GLOBAL_BOOKING_DATE',
+            'GLOBAL_PREPARATION_TIME'
+        );
+
+        foreach ($keysToRemove as $keyToRemove) {
+            Configuration::deleteByName($keyToRemove);
+        }
+
+        return true;
+    }
+
+    public function getNumberOfDays($dateFrom, $dateTo)
+    {
+        $startDate = new DateTime($dateFrom);
+        $endDate = new DateTime($dateTo);
+        $daysDifference = $startDate->diff($endDate)->days;
+
+        return $daysDifference;
+    }
+
+    public function installTab($class_name, $tab_name, $tab_parent_name = false, $need_tab = true)
+    {
+        $tab = new Tab();
+        $tab->active = 1;
+        $tab->class_name = $class_name;
+        $tab->name = array();
+
+        foreach (Language::getLanguages(true) as $lang) {
+            $tab->name[$lang['id_lang']] = $tab_name;
+        }
+
+        if ($tab_parent_name) {
+            $tab->id_parent = (int) Tab::getIdFromClassName($tab_parent_name);
+        } elseif (!$need_tab) {
+            $tab->id_parent = -1;
+        } else {
+            $tab->id_parent = 0;
+        }
+
+        $tab->module = $this->module->name;
+        $res = $tab->add();
+
+        return $res;
+    }
+
+    public function createHotelDefaultBedTypes()
+    {
+        $htlBedTypes = array(
+            array(
+                'length' => '6.25',
+                'width'  => '3.16',
+                'name' => array(
+                    'en' => 'Twin Bed',
+                    'nl' => 'Eenpersoonsbed',
+                    'fr' => 'Lit simple',
+                    'de' => 'Einzelbett',
+                    'ru' => 'Односпальная кровать',
+                    'es' => 'Cama individual',
+                ),
+            ),
+            array(
+                'length' => '6.66',
+                'width'  => '3.16',
+                'name' => array(
+                    'en' => 'Twin XL Bed',
+                    'nl' => 'Eenpersoonsbed XL',
+                    'fr' => 'Lit simple XL',
+                    'de' => 'Einzelbett XL',
+                    'ru' => 'Односпальная кровать XL',
+                    'es' => 'Cama individual XL',
+                ),
+            ),
+            array(
+                'length' => '6.25',
+                'width'  => '4.5',
+                'name' => array(
+                    'en' => 'Full Bed',
+                    'nl' => 'Tweepersoonsbed',
+                    'fr' => 'Lit double',
+                    'de' => 'Doppelbett',
+                    'ru' => 'Двуспальная кровать',
+                    'es' => 'Cama doble',
+                ),
+            ),
+            array(
+                'length' => '6.66',
+                'width'  => '5',
+                'name' => array(
+                    'en' => 'Queen Bed',
+                    'nl' => 'Queen size bed',
+                    'fr' => 'Lit Queen',
+                    'de' => 'Queen-Size-Bett',
+                    'ru' => 'Кровать Queen Size',
+                    'es' => 'Cama Queen',
+                ),
+            ),
+            array(
+                'length' => '6.66',
+                'width'  => '6.33',
+                'name' => array(
+                    'en' => 'King Bed',
+                    'nl' => 'King size bed',
+                    'fr' => 'Lit King',
+                    'de' => 'King-Size-Bett',
+                    'ru' => 'Кровать King Size',
+                    'es' => 'Cama King',
+                ),
+            ),
+            array(
+                'length' => '7',
+                'width'  => '6',
+                'name' => array(
+                    'en' => 'California King Bed',
+                    'nl' => 'California King bed',
+                    'fr' => 'Lit California King',
+                    'de' => 'California King-Bett',
+                    'ru' => 'Калифорнийская кровать King Size',
+                    'es' => 'Cama California King',
+                ),
+            ),
+            array(
+                'length' => '6.25',
+                'width'  => '3.16',
+                'name' => array(
+                    'en' => 'Bunk Bed',
+                    'nl' => 'Stapelbed',
+                    'fr' => 'Lit superposé',
+                    'de' => 'Etagenbett',
+                    'ru' => 'Двухъярусная кровать',
+                    'es' => 'Litera',
+                ),
+            ),
+            array(
+                'length' => '6.25',
+                'width'  => '4.5',
+                'name' => array(
+                    'en' => 'Sofa Bed',
+                    'nl' => 'Slaapbank',
+                    'fr' => 'Canapé-lit',
+                    'de' => 'Schlafsofa',
+                    'ru' => 'Диван-кровать',
+                    'es' => 'Sofá cama',
+                ),
+            ),
+            array(
+                'length' => '6.66',
+                'width'  => '5',
+                'name' => array(
+                    'en' => 'Murphy Bed',
+                    'nl' => 'Inklapbed',
+                    'fr' => 'Lit escamotable',
+                    'de' => 'Klappbett',
+                    'ru' => 'Откидная кровать',
+                    'es' => 'Cama abatible',
+                ),
+            ),
+        );
+
+        $languages = Language::getLanguages(true);
+        foreach ($htlBedTypes as $htlBedType) {
+            $objBedType = new HotelBedType();
+            foreach ($languages as $lang) {
+                if (isset($htlBedType['name'][$lang['iso_code']])) {
+                    $objBedType->name[$lang['id_lang']] = $htlBedType['name'][$lang['iso_code']];
+                } else {
+                    $objBedType->name[$lang['id_lang']] = $htlBedType['name']['en'];
+                }
+
+                $objBedType->width = $htlBedType['width'];
+                $objBedType->length = $htlBedType['length'];
+                $objBedType->save();
+            }
+        }
+
+        return true;
+    }
+
+    public function updateTables()
+    {
+        if ($sql = $this->getModuleSql()) {
+            foreach ($sql as $query) {
+                if ($query) {
+                    if (!Db::getInstance()->execute(trim($query))) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public function getModuleSql()
+    {
+        $sql = array(
+            "ALTER TABLE `"._DB_PREFIX_."htl_branch_info`
+                ADD COLUMN `fax` varchar(255) DEFAULT NULL AFTER `active_refund`;",
+
+            "ALTER TABLE `"._DB_PREFIX_."htl_booking_detail`
+                ADD COLUMN `planned_check_out` DATETIME NOT NULL `check_out`;",
+
+            "UPDATE `"._DB_PREFIX_."htl_booking_detail`
+                SET `planned_check_out` = `date_to`",
+
+            "ALTER TABLE `"._DB_PREFIX_."htl_order_restrict_date`
+                CHANGE COLUMN `use_global_max_order_date` `use_global_max_checkout_offset` TINYINT(1) NOT NULL,
+                CHANGE COLUMN `use_global_preparation_time` `use_global_min_booking_offset` TINYINT(1) NOT NULL,
+                CHANGE COLUMN `preparation_time` `min_booking_offset` INT(11) NOT NULL,
+                ADD COLUMN `max_checkout_offset` INT(11) NOT NULL AFTER `max_order_date`;",
+        );
+
+        $maxCheckoutOffset = Configuration::get('PS_MAX_CHECKOUT_OFFSET');
+        $dateToday = date('Y-m-d');
+        if ($hotelRestrictInfos = Db::getInstance()->executeS("SELECT *  FROM `"._DB_PREFIX_."htl_order_restrict_date`")) {
+            $restrictSql = 'UPDATE `'._DB_PREFIX_.'htl_order_restrict_date` SET max_checkout_offset = CASE';
+            foreach ($hotelRestrictInfos as $hotelRestrictInfo) {
+                if ($hotelRestrictInfo['use_global_max_checkout_offset']) {
+                    $restrictSql .= ' WHEN `id` = '.(int)$hotelRestrictInfo['id'].' AND `id_hotel` = '.(int)$hotelRestrictInfo['id_hotel'].'
+                        THEN '.(int) $maxCheckoutOffset;
+                } else {
+                    $checkoutOffset = $this->getNumberOfDays($dateToday, $hotelRestrictInfo['max_order_date']);
+                    $restrictSql .= ' WHEN `id` = '.(int)$hotelRestrictInfo['id'].' AND `id_hotel` = '.(int)$hotelRestrictInfo['id_hotel'].'
+                        THEN '.(int) $checkoutOffset;
+                }
+            }
+
+            $restrictSql .= ' END';
+            $sql[] = $restrictSql;
+        }
+
+        $sql[] = "ALTER TABLE `"._DB_PREFIX_."htl_order_restrict_date`
+            DROP COLUMN `max_order_date`";
+
+        return $sql;
+    }
+}
